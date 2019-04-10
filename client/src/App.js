ed58c6be86ed824ce83524b86b9c141893863415
Christopher <christopher.carnivale@gmail.com>
2019-04-10 12:27:34 -0400

    #3 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-
-
 class App extends Component {
   
   constructor(props){
--- search.js
+++ search.js
@@ -1,2 +1,27 @@
 //1.  a. 11 -> 5 -> 6 -> 8
 //    b. 11 -> 15 -> 17 -> not found
+
+//3.
+/* 
+Imagine you are looking for a book in a library with a Dewey Decimal index. 
+How would you go about it? Can you express this process as a search algorithm? 
+Binary Search
+
+function findBook(dewey, bookValue, start, end){
+  if (start > end){
+    return 'Book not found'
+  }
+  const middle = Math.floor((start + end) / 2);
+  
+  if (bookValue = middle){
+    return bookValue
+  }
+  else if (bookValue > middle){
+    return findBook(dewey, bookValue, middle+1, end)
+  }
+  else if (bookValue < middle){
+    return findBook(dewey, bookValue, start, middle - 1)
+  }
+}
+Implement your algorithm to find a book whose Dewey and book title is provided.
+*/
